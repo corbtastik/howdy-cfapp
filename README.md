66986465de20b72745a6547a5289b1c654ea8bf9
Corbett Martin <corbtastik@gmail.com>
2019-01-22 12:03:51 -0600

    first push

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cloud Foundry App
+
+Small demo application for Cloud Foundry, implemented in...
+ 
+* [Spring Boot](https://spring.io/projects/spring-boot) for app
+* wit [Mustache](https://mustache.github.io/) for web templating
+* and [Bulma](https://bulma.io) for layouts in flexbox
+
+## Instructions
+
+1. clone or fork this repo
+1. ./mvnw clean package
+1. cf push
+1. access app endpoint
+
---
+++ src/main/java/io/cfapp/SiteController.java
@@ -0,0 +1,50 @@
+package io.cfapp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Controller
+public class SiteController {
+
+    @Value("${site.title}")
+    private String title = "";
+
+    @Value("${site.subtitle}")
+    private String subtitle = "";
+
+    @Value("${VCAP_APPLICATION:{}}")
+    private String appInfo;
+
+    @Value("${VCAP_SERVICES:{}}")
+    private String serviceInfo;
+
+    private final ObjectMapper json;
+
+    @Autowired
+    public SiteController(ObjectMapper json) {
+        this.json = json;
+    }
+
+    @GetMapping("/site")
+	public String site(Map<String, Object> model) {
+		model.put("title", title);
+		model.put("subtitle", subtitle);
+		return "site";
+	}
+
+    @GetMapping("/")
+    public String index(Map<String, Object> model) throws IOException {
+        model.put("title", title);
+        model.put("subtitle", subtitle);
+        model.put("cfapp", json.readValue(appInfo, LinkedHashMap.class));
+        model.put("cfservices", json.readValue(serviceInfo, LinkedHashMap.class));
+        return "site";
+    }
+}
